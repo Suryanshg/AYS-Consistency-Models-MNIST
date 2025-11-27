4c94ce57d861c61408c80cf0519a2ed3268d5d21
Suryansh Goyal <suryanshg47@gmail.com>
2025-11-27 14:11:09 -0500

    Added code in main.py as an example of loading MNIST dataloader

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+from datasets.mnist_dataloader import get_mnist_dataloader
+
+
 def main():
     print("Hello from cs-552-generative-ai-final-project!")
+
+    # Load MNIST Dataloader
+    mnist_dataloader = get_mnist_dataloader()
+    
+    # Print Dataset Summary
+    print(f"{'-' * 10} DATASET INFO {'-' * 10}\n")
+    print(f"Dataset length: {len(mnist_dataloader.dataset)}")
+    print(f"X (Image) shape: {mnist_dataloader.dataset[0][0].shape}")
+    print(f"{'-' * 34}")
 
 
 if __name__ == "__main__":
